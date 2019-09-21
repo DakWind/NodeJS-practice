7d7375c06f5fa9482d7c28a59358ec77c52d2fdb
Aku Pyykkönen <akupyy@gmail.com>
2019-09-21 15:10:48 +0300

    first commit

---
+++ public/all.js
@@ -0,0 +1,35 @@
+getData();
+
+// form the map
+
+const mymap = L.map('mapid').setView([0, 0], 0);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+const tiles = L.tileLayer(tileURL, { attribution });
+tiles.addTo(mymap);
+
+async function getData() {
+    const response = await fetch('/api');
+    const data = await response.json();
+
+    for (item of data) {
+        const marker = L.marker([item.lat, item.lon]).addTo(mymap);
+
+        const root = document.createElement('div');
+        const timestamp = document.createElement('div');
+        const geo = document.createElement('div');
+        
+        const timestampString = new Date(item.timestamp).toLocaleString();
+        timestamp.textContent = timestampString
+        geo.textContent = `geo: ${item.lat}, ${item.lon}`;
+
+        const txt = `latitude: ${item.lat}; longitude: ${item.lon}; time: ${timestampString}`
+
+        marker.bindPopup(txt);
+
+        root.append(timestamp, geo);
+        document.body.append(root);
+    }
+    console.log(data);
+}
---
+++ public/geolocation.js
@@ -0,0 +1,47 @@
+let lat;
+let lon;
+const button = document.getElementById("submit");
+
+button.addEventListener('click', async event => {
+const data = { lat, lon };
+const options = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+};
+const response = await fetch('/api', options);
+const json = await response.json();
+//console.log(json);
+});
+
+// form the map
+
+const mymap = L.map('mapid').setView([51.505, -0.09], 3);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+const tiles = L.tileLayer(tileURL, { attribution });
+tiles.addTo(mymap);
+
+// make the marker
+
+const marker = L.marker([0, 0]).addTo(mymap);
+
+// get location and move map
+
+if ("geolocation" in navigator) {
+    console.log('geolocation available');
+    navigator.geolocation.getCurrentPosition(async position => {
+        lat = position.coords.latitude;
+        document.getElementById('latitude').textContent = lat.toFixed(2);
+        lon = position.coords.longitude;
+        document.getElementById('longitude').textContent = lon.toFixed(2);
+        marker.setLatLng([lat, lon]);
+        mymap.flyTo([lat, lon]);
+        });
+} else {
+  console.log('geolocation not available');
+}
+
