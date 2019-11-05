0d3a0bec72bb2fea01e3d1fe3878a631d6da4e5c
Aku Pyykkönen <akupyy@gmail.com>
2019-11-05 19:30:35 +0200

    zoomaus

--- public/all.js
+++ public/all.js
@@ -2,7 +2,7 @@ getData();
 
 // form the map
 
-const mymap = L.map('mapid').setView([0, 0], 0);
+const mymap = L.map('mapid').setView([44.42, 26.10], 2);
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
